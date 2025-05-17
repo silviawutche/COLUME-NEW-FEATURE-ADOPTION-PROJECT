2c68a0cf02f48543d5b837b6cd83c2ced1ddcf94
silvia wutche <sylviawutche@gmail.com>
2025-05-17 19:16:24 +0100

    Update ANALYSIS.sql

--- ANALYSIS.sql
+++ ANALYSIS.sql
@@ -74,8 +74,7 @@ user_group AS (
     JOIN activity_log a ON a.user_id = all_users.eligible_users
     WHERE timestamp BETWEEN DATEADD(WEEK, -2, '2025-02-20') AND DATEADD(WEEK, 6, '2025-02-20')
 ),
-
--- Count total users per group (adopter vs non-adopter)
+--Count total users per group (adopter vs non-adopter)
 grouped AS (
     SELECT adopter_group, COUNT(DISTINCT eligible_users) AS all_users
     FROM user_group
@@ -103,8 +102,8 @@ retention_rate AS (SELECT
    ROUND(CAST(retained_users * 100.0 AS FLOAT) / all_users,2) AS retention_rate
 FROM Weekly_retention wr 
 JOIN grouped g ON g.adopter_group = wr.adopter_group
-),
---SELECT * FROM retention_rate
+)
+SELECT * FROM retention_rate
 
 pivoted AS (
 SELECT week_diff,
@@ -168,6 +167,7 @@ SELECT * FROM user_weekly
 
 -- UPGRADE STATUS
 CREATE VIEW Plan_change AS 
+
 WITH eligible_users AS (
     SELECT user_id 
     FROM users
@@ -182,45 +182,40 @@ adopters AS (
     WHERE activity_type IN ('task_reminder','voice_assistant', 'custom_theme')
       AND timestamp BETWEEN '2025-02-20' AND DATEADD(DAY, 7,'2025-02-20')
       AND user_id IN (SELECT user_id FROM eligible_users)
-)--,
-
---billing_check AS (
-SELECT eu.user_id, b.billing_date,plan_type, 
-CASE WHEN a.user_id IS NOT NULL THEN 'adopter' ELSE 'non_adopter' END AS adopter_group,
-ROW_NUMBER() OVER (PARTITION BY eu.user_id ORDER BY billing_date) RowN
-FROM eligible_users eu
-LEFT JOIN adopters a ON eu.user_id = a.user_id
-LEFT JOIN billing b ON eu.user_id = b.user_id
-WHERE billing_date BETWEEN DATEADD(DAY,-30,'2025-02-20') AND DATEADD(DAY,30,'2025-02-20')
 ),
 
-first_bill AS (SELECT * FROM billing_check
-WHERE RowN = 1),
 
-second_bill AS (SELECT * FROM billing_check
-WHERE RowN = 2)
+first_billing AS (SELECT user_id,plan_type, MIN(billing_date) AS first_billing
+FROM billing
+WHERE billing_date BETWEEN DATEADD(DAY,-30,'2025-02-20') AND '2025-02-20'
+GROUP BY user_id,plan_type),
 
---plan_change AS (
-SELECT f.user_id, f.adopter_group, f.billing_date AS first_billing,
-f.plan_type AS first_plan, s.billing_date AS second_billing,
-s.plan_type AS second_plan
-FROM first_bill f 
-JOIN second_bill s ON f.user_id = s.user_id
+second_billing AS (SELECT user_id,plan_type, MIN(billing_date) AS second_billing
+FROM billing
+WHERE billing_date BETWEEN '2025-02-20' AND DATEADD(DAY,30,'2025-02-20')
+GROUP BY user_id,plan_type),
 
 
+Billing_check AS (SELECT eu.user_id,
+CASE WHEN a.user_id IS NOT NULL THEN 'adopter' ELSE 'non_adopter' END AS adopter_group,
+f.first_billing AS first_billing_date,f.plan_type AS first_plan,
+s.second_billing AS second_billing_date, s.plan_type AS second_plan
+FROM eligible_users eu
+LEFT JOIN adopters a ON eu.user_id = a.user_id
+LEFT JOIN first_billing f ON f.user_id = eu.user_id
+LEFT JOIN second_billing s ON s.user_id = eu.user_id
+WHERE f.first_billing IS NOT NULL
+AND f.plan_type <> 'Enterprise')
 
 
-/* CASE WHEN f.plan_type = s.plan_type THEN 'Same_plan'
-WHEN f.plan_type IN ('Basic','Pro') AND  s.plan_type IN ('Pro', 'Enterprise') THEN 'Upgraded'
-WHEN f.plan_type = 'Pro' AND  s.plan_type = 'Basic' THEN 'Down_graded' END AS plan_change
-FROM first_bill f */
-JOIN second_bill s ON f.user_id = s.user_id)
+SELECT user_id,adopter_group,
+CASE WHEN first_plan = second_plan THEN 'same_plan'
+WHEN second_billing_date IS NULL AND second_plan IS NULL THEN 'No renewal'
+WHEN first_plan IN ('Basic','Pro') AND  second_plan IN ('Pro', 'Enterprise') THEN 'Upgraded'
+WHEN first_plan = 'Pro' AND   second_plan = 'Basic' THEN 'Down_graded' END AS plan_change
+FROM Billing_check
 
-SELECT adopter_group, COUNT(CASE WHEN plan_change = 'Same_plan' THEN 1 ELSE NULL  END) AS same_plan_cnt,
-COUNT(CASE WHEN plan_change = 'Upgraded' THEN 1 ELSE NULL  END) AS Upgrade_cnt,
-COUNT(CASE WHEN plan_change = 'Down_graded' THEN 1  ELSE NULL END ) AS DownGrade_plan_cnt
-FROM plan_change
-GROUP BY adopter_group
+
 
 
 SELECT * FROM retention_rate
@@ -244,11 +239,52 @@ adopters AS (
       AND user_id IN (SELECT user_id FROM eligible_users)
 )
 
-SELECT COUNT(eu.user_id) all_users,COUNT(a.user_id) adopter_cnt, COUNT(CASE WHEN a.user_id IS NULL THEN 1 END) AS non_adopter_cnt,
-ROUND(CAST(COUNT(a.user_id) AS FLOAT) * 100.0/COUNT(eu.user_id),2) AS adoption_rate FROM eligible_users eu
+--SELECT * FROM adopters
+
+SELECT COUNT(eu.user_id) all_users,COUNT(a.user_id) adopter_cnt, 
+COUNT(CASE WHEN a.user_id IS NULL THEN 1 END) AS non_adopter_cnt,
+ROUND(CAST(COUNT(a.user_id) AS FLOAT) * 100.0/COUNT(eu.user_id),2) AS adoption_rate 
+FROM eligible_users eu
 LEFT JOIN adopters a ON a.user_id = eu.user_id;
 
 SELECT * FROM Plan_change
 
+CREATE VIEW weekly_login AS
+WITH eligible_users AS (
+    SELECT user_id 
+    FROM users
+    WHERE sign_up_date < '2025-02-20' 
+      AND (churn_date > '2025-02-20' OR churn_date IS NULL)
+),
 
-SELECT  FROM Plan_change
+-- Identify users who engaged with new features in the first 7 days after launch
+adopters AS (
+    SELECT DISTINCT user_id 
+    FROM activity_log
+    WHERE activity_type IN ('task_reminder','voice_assistant', 'custom_theme')
+      AND timestamp BETWEEN '2025-02-20' AND DATEADD(DAY, 7,'2025-02-20')
+      AND user_id IN (SELECT user_id FROM eligible_users)
+),
+all_users AS (
+    SELECT 
+        eu.user_id as eligible_users, 
+        CASE WHEN a.user_id IS NOT NULL THEN 'adopter' ELSE 'non_adopter' END AS adopter_group
+    FROM eligible_users eu 
+    LEFT JOIN adopters a ON eu.user_id = a.user_id
+),
+activity_table AS (SELECT eligible_users, adopter_group,timestamp,DATEDIFF(WEEK, '2025-02-20',timestamp) AS week
+FROM all_users al
+JOIN activity_log a ON al.eligible_users = a.user_id
+WHERE timestamp BETWEEN DATEADD(WEEK,-2,'2025-02-20') AND DATEADD(WEEK,6,'2025-02-20')),
+
+user_weekly  AS (SELECT eligible_users,adopter_group,timestamp,week ,
+ROW_NUMBER() OVER (PARTITION BY eligible_users, week ORDER BY timestamp) AS rn
+FROM activity_table)
+
+SELECT eligible_users,adopter_group,timestamp
+FROM user_weekly 
+WHERE rn = 1
+
+SELECT activity_id FROM activity_log
+
+CREATE INDEX act_timestamp ON activity_log (timestamp)
